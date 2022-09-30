9447de1c579ac6746e2f53165caa167bc1cdf7cd
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2022-09-30 14:22:43 -0700

    Update 5_Comtrade_Charts.py

--- pages/5_Comtrade_Charts.py
+++ pages/5_Comtrade_Charts.py
@@ -25,7 +25,10 @@ color_dict = {
             "Preparations of cereals, flour, starch or milk; pastrycooks' products":"#662C69",
             "Fish and crustaceans, molluscs and other aquatic invertebrates":"#A3AAA5",
             "Meat, fish or crustaceans, molluscs or other aquatic invertebrates; preparations thereof":"#3B768C",
-            "Preparations of vegetables, fruit, nuts or other parts of plants":"#E95F85"
+            "Preparations of vegetables, fruit, nuts or other parts of plants":"#E95F85",
+            "Import": "#3C787E",
+            "Export": "#241623"
+
             }
 
 def get_comtrade_data(endpoint_url):
@@ -50,6 +53,13 @@ def make_yoygraph (dataframe, Type):
     fig.update_traces(marker_line_color='white', marker_line_width=0.0)
     fig.update_layout(margin = dict(t=50, l=0, r=5, b=0))
     return(fig)
+def make_sunburst_chart (df1, df2):
+    # join df1 and df2
+    total_df = pd.concat([df1, df2])
+    fig = px.sunburst(total_df, path=['rgDesc', 'cmdDescE'], values='TradeValue', color='cmdDescE', color_discrete_map=color_dict, title="Imports and Exports by Commodity")
+    fig.update_layout(margin = dict(t=50, l=0, r=0, b=0))
+    # fig.update_traces(color_discrete_map=color_dict, title='Total Imports and Exports')
+    return(fig)
 
 def commodity_charts(country_code):
     year = "2021"
@@ -58,19 +68,24 @@ def commodity_charts(country_code):
     food_code_string = "01%2C02%2C03%2C04%2C05%2C07%2C08%2C09%2C10%2C11%2C12%2C13%2C15%2C16%2C17%2C19%2C20"
     import_url = f"http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps={year}&r={country_code}&p=all&rg=1&cc={food_code_string}"
     export_url = f"http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps={year}&r={country_code}&p=all&rg=2&cc={food_code_string}"
-    yoy_import_url = f"http://comtrade.un.org/api/get?max=502&type=C&freq=A&px=HS&ps={multiyear_string}&r={country_code}&p=0&rg=1&cc={food_code_string}"
-    yoy_export_url = f"http://comtrade.un.org/api/get?max=502&type=C&freq=A&px=HS&ps={multiyear_string}&r={country_code}&p=0&rg=2&cc={food_code_string}"
+    yoy_import_url = f"http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps={multiyear_string}&r={country_code}&p=0&rg=1&cc={food_code_string}"
+    yoy_export_url = f"http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps={multiyear_string}&r={country_code}&p=0&rg=2&cc={food_code_string}"
     import_df = get_comtrade_data(import_url)
+    print (import_url)
     import_df = import_df[import_df['ptTitle'].str.match('World') == False]
     export_df = get_comtrade_data(export_url)
+    print (export_url)
     export_df = export_df[export_df['ptTitle'].str.match('World') == False]
     yoy_import_df = get_comtrade_data(yoy_import_url)
+    print (yoy_import_url)
     yoy_export_df = get_comtrade_data(yoy_export_url)
+    print (yoy_export_url)
     import_treegraph = make_treegraph(import_df, year, "Imports")
     export_treegraph = make_treegraph(export_df, year, "Exports")
     yoy_import_graph = make_yoygraph(yoy_import_df, "Imports")
     yoy_export_graph = make_yoygraph(yoy_export_df, "Exports")
-    return [import_treegraph, yoy_import_graph, export_treegraph, yoy_export_graph]
+    total_sunburstchart = make_sunburst_chart (import_df, export_df)
+    return [import_treegraph, yoy_import_graph, export_treegraph, yoy_export_graph, total_sunburstchart]
     
 page_title = st.sidebar.empty()
 page_title.header("Comtrade Data Overviews")
@@ -93,11 +108,18 @@ region = st.selectbox(
 
 if st.button('Make Charts'):
     country_code = country_df.loc[country_df['text'] == region, 'id'].item()
+    charts = commodity_charts(country_code)
+    # st.plotly_chart(charts[4], use_container_width=True)
+    # st.plotly_chart(charts[0], use_container_width=True)
+    # st.plotly_chart(charts[1], use_container_width=True)
+    # st.plotly_chart(charts[2], use_container_width=True)
+    # st.plotly_chart(charts[3], use_container_width=True)
     if region == "World" or region == "All":
         st.write(f"{region} is not a valid country")
     else:
         try:
             charts = commodity_charts(country_code)
+            st.plotly_chart(charts[4], use_container_width=True)
             st.plotly_chart(charts[0], use_container_width=True)
             st.plotly_chart(charts[1], use_container_width=True)
             st.plotly_chart(charts[2], use_container_width=True)
@@ -141,3 +163,7 @@ if st.button('Make Charts'):
 #      else:
 #           st.write("No data found for this query")
 
+
+
+# n = "http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps=2021&r=586&p=all&rg=2&cc=01%2C02%2C03%2C04%2C05%2C07%2C08%2C09%2C10%2C11%2C12%2C13%2C15%2C16%2C17%2C19%2C20"
+# y = "http://comtrade.un.org/api/get?max=1999&type=C&freq=A&px=HS&ps=2021&r=586&p=all&rg=1&cc=01%2C02%2C03%2C04%2C05%2C07%2C08%2C09%2C10%2C11%2C12%2C13%2C15%2C16%2C17%2C19%2C20"
