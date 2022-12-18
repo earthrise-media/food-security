bb8f9ffc651a8ed4800237d782e178801f76185b
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2022-12-17 17:05:18 -0700

    updating webmap with historical data
    should close #28

--- hostedcontent/fewsnet-pop/index.js
+++ hostedcontent/fewsnet-pop/index.js
@@ -1,3 +1,50 @@
+reportDates = [
+  "CS-2009-07",
+  "CS-2009-10",
+  "CS-2010-01",
+  "CS-2010-04",
+  "CS-2010-07",
+  "CS-2010-10",
+  "CS-2011-01",
+  "CS-2011-04",
+  "CS-2011-07",
+  "CS-2011-10",
+  "CS-2012-01",
+  "CS-2012-04",
+  "CS-2012-07",
+  "CS-2012-10",
+  "CS-2013-01",
+  "CS-2013-04",
+  "CS-2013-07",
+  "CS-2013-10",
+  "CS-2014-01",
+  "CS-2014-04",
+  "CS-2014-07",
+  "CS-2014-10",
+  "CS-2015-01",
+  "CS-2015-04",
+  "CS-2015-07",
+  "CS-2015-10",
+  "CS-2016-02",
+  "CS-2016-06",
+  "CS-2016-10",
+  "CS-2017-02",
+  "CS-2017-06",
+  "CS-2017-10",
+  "CS-2018-02",
+  "CS-2018-06",
+  "CS-2018-10",
+  "CS-2018-12",
+  "CS-2019-02",
+  "CS-2019-06",
+  "CS-2019-10",
+  "CS-2020-02",
+  "CS-2020-06",
+  "CS-2020-10",
+  "CS-2021-02",
+  "CS-2021-06",
+  "CS-2021-10",
+];
 mapboxgl.accessToken =
   "pk.eyJ1IjoiaGlnaGVzdHJvYWQiLCJhIjoiY2w4YWRueTN5MDRhZjNvbWhmb2hlNXFsZyJ9.o7eX3yCdCqUt0VZxpofVoQ";
 const map = new mapboxgl.Map({
@@ -32,6 +79,7 @@ var standardStyle = [
 // map.setFilter('fewsnet', ['==', 'report', reportvar])
 
 map.on("load", () => {
+
   $('input[type="checkbox"]').change(function () {
     var checkboxName = $(this).attr("name");
     if (this.checked) {
@@ -58,12 +106,53 @@ map.on("load", () => {
       }
       // checkbox was unchecked
       console.log("Checkbox with name '" + checkboxName + "' was unchecked");
-    };
+    }
   });
+
+  $(function () {
+    $("#range-slider input[type='range']").on("input", function () {
+      console.log(this.value);
+      $("#range-value").html("Report Date: " + reportDates[this.value].slice(8, 10) + "/" + reportDates[this.value].slice(3, 7));
+      // console.log the 3-5 characters of reportDates[this.value]
+      console.log(reportDates[this.value]);      // update paint settings of population layer
+      map.setPaintProperty(
+        "population Layer",
+        "circle-color",
+        [
+          "match",
+          ["get", reportDates[this.value]],
+          [1],
+          "#a4cdad",
+          [2],
+          "hsl(52, 98%, 70%)",
+          [3],
+          "hsl(28, 98%, 56%)",
+          [4],
+          "hsl(0, 100%, 43%)",
+          "hsla(0, 0%, 62%, 0)"
+      ]
+      );
+    });
+  });
+  // console.log length of reportDates
+  console.log("last report " + reportDates[44]);
+  // set max range of slider equal to length of reportDates
+  $("#range-slider input[type='range']").attr("max", reportDates.length - 1);
+  $("#range-slider input[type='range']").attr("value", reportDates.length - 1);
+  $("#range-value").html("Report Date: " + reportDates[reportDates.length - 1].slice(8, 10) + "/" + reportDates[reportDates.length - 1].slice(3, 7));
+  $("#date-id").html(reportDates[reportDates.length - 1].slice(8, 10) + "/" + reportDates[reportDates.length - 1].slice(3, 7));
+
+
+
   map.on("click", "population Layer", (e) => {
     new mapboxgl.Popup()
       .setLngLat(e.lngLat)
-      .setHTML("Population: " + e.features[0].properties.population + "<br>IPC Rating: " + e.features[0].properties["CS-2018-10"])
+      .setHTML(
+        "Population: " +
+          e.features[0].properties.population +
+          "<br>IPC Rating: " +
+          e.features[0].properties["CS-2018-10"]
+      )
       .addTo(map);
   });
 
