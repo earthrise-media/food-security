e5ee5b9cb1ca9634d8b4b2fc43036aae07366dfe
Mikel Maron <mikelmaron@Mikels-Air.verizon.net>
2023-01-23 20:51:19 -0500

    add a chart to the fewsnet population demo

--- hostedcontent/fewsnet-pop/index.js
+++ hostedcontent/fewsnet-pop/index.js
@@ -155,6 +155,15 @@ map.on("load", () => {
           e.features[0].properties[reportDates[$("#range-slider input[type='range']").val()]]
       )
       .addTo(map);
+
+    var chartData = reportDates.map((date) => {
+      var val = e.features[0].properties[date];
+      if (val > 5 || val == undefined) {
+        val = null;
+      }      
+      return val;
+    });
+    chart.load({columns: [['data'].concat(chartData)]});
   });
 
   // Change the cursor to a pointer when
@@ -168,4 +177,34 @@ map.on("load", () => {
   map.on("mouseleave", "population Layer", () => {
     map.getCanvas().style.cursor = "";
   });
+});
+
+const chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    columns: [['data'].concat(reportDates)],
+    names: { data: "IPC Phase" },
+    type: 'line',
+  },
+  axis: {
+    x: {
+      type: 'category',
+      categories: reportDates.map(x => x.replace("CS-", "").replace(/-\d\d/,"")),
+      tick: { count: 5, rotate: 0, culling: false },
+      height: 50
+    },
+    y: { max: 5, min: 0, tick: {count: 6, values: [0,1,2,3,4,5] }}
+  },
+  size: {
+    height: 160,
+  },
+  line: {
+    connect_null: false
+  },
+  point: {
+    show: false
+  },
+  legend: {
+    show: false
+  }
 });
